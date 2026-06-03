1cc290344b4aaf3a55e0d19e53eee995261f5272
missmioh <186388075+missmioh@users.noreply.github.com>
2026-06-03 14:36:33 +0200

    Fix app name and add function file

---
+++ lib/view_models/app_view_model.dart
@@ -0,0 +1,27 @@
+// Ablageort für Funktionen und Farben
+
+import 'package:flutter/material.dart';
+
+// Notwendig für Task-Speicherung
+
+class Task {
+  String title;
+  bool complete;
+
+  Task(this.title, this.complete);
+}
+
+class AppViewModel extends ChangeNotifier {
+  List<Task> tasks = <Task>[];
+
+// Farb-Schemata
+
+  Color colorLight = Colors.amber.shade100;
+  Color colorMedium = Colors.amber.shade200;
+  Color colorDark = Colors.amber.shade300;
+  Color colorDarkest = Colors.amber.shade600;
+  Color get colorText => const Color.fromARGB(255, 163, 116, 6);
+  Color get colorAccent1 => const Color.fromARGB(179, 192, 9, 6);
+  Color get colorAccent2 => const Color.fromARGB(255, 141, 41, 5);
+
+}
