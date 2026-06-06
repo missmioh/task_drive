3269318432bbd7b1061808ef0872f21681704a82
Oskar <nema.oskar@gmail.com>
2026-06-06 18:05:08 +0200

    dbug Schriftzug ausgeblendet

--- lib/main.dart
+++ lib/main.dart
@@ -1,44 +1,32 @@
 import 'package:flutter/material.dart';
 
 void main() {
-
-  runApp( AddictiveTasks() );
+  runApp(AddictiveTasks());
 }
 
 class AddictiveTasks extends StatelessWidget {
-  const AddictiveTasks ({super.key});
+  const AddictiveTasks({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false, // Schriftzug rechte Ecke deaktiviert
       home: Scaffold(
-        appBar: AppBar(backgroundColor: Colors.amber.shade600,
-        title: const Text('Addictive Tasks'),
+        appBar: AppBar(
+          backgroundColor: Colors.amber.shade600,
+          title: const Text('Addictive Tasks'),
         ),
         body: Column(
           children: [
-
             // header, wo z.B. User-Daten stehen könnten
+            Expanded(flex: 1, child: Container(color: Colors.amber.shade100)),
 
-            Expanded(
-              flex: 1,
-              child: Container(color: Colors.amber.shade100),
-            ),
-            
             // Infos über die Tasks (z.B. wie viele übrig sind)
             // Könnte aber auch für die Kategorien-Unterseite sein
-
-            Expanded(
-              flex: 1,
-              child: Container(color: Colors.amber.shade200),
-            ),
+            Expanded(flex: 1, child: Container(color: Colors.amber.shade200)),
 
             // Ansicht der Listen-Elemente
-
-            Expanded(
-              flex: 7,
-              child: Container(color: Colors.amber.shade300),
-            ),
+            Expanded(flex: 7, child: Container(color: Colors.amber.shade300)),
           ],
         ),
 
